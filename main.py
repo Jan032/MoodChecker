5ea32a4038c09c10fe09b9cc36206d24d5a656e7
Jan Krajnc <jan.krajnc17@gmail.com>
2021-10-08 13:00:14 +0200

    initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+mood = input("how are you feeling?: ")
+
+if mood == "happy":
+    print("It's good to see you happy!")
+elif mood == "sad":
+    print("it's ok, you're ok :)")
+elif mood == "nervous":
+    print("take a deep breath and relax...")
+elif mood == "angry":
+    print("Go fck em up!")
+else:
+    print("I'm not programmed for your request, try: happy, sad, nervous, angry")
